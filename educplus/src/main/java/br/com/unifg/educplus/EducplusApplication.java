f978358d71c47af9f841924eda7169652359041c
Jose Robson <joserobsonsiqueira23@hotmail.com>
2023-10-31 13:36:48 -0300

    create project

---
+++ educplus/src/main/java/br/com/unifg/educplus/EducplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifg.educplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducplusApplication.class, args);
+	}
+
+}
---
+++ educplus/src/test/java/br/com/unifg/educplus/EducplusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifg.educplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
